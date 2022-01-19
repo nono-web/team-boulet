4dc4ca6d012e7050596f87951e54f3318e9c8e7d
Paul-Verdure <paul.verdure@gmail.com>
2022-01-19 20:27:06 +0100

    fichier data + exemple Readme

--- team-boulet/src/Components/Home/Home.jsx
+++ team-boulet/src/Components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-    <div>Hello</div>
+    <div></div>
 )};
 
 export default Home;
--- team-boulet/src/Components/Materials/Materials.jsx
+++ team-boulet/src/Components/Materials/Materials.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 const Materials = () => {
     return (
-    <div className='m'>
-        <h1> 
+    <div className='materials'>
 
     </div>
 )};
