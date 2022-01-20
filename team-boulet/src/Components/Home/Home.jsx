125095737a946bf1ee52df8396cc10f77c7cc48c
Tadkozh <tad.kozh.bihan@free.fr>
2022-01-20 18:08:05 +0100

    minor errors

--- team-boulet/src/Components/Home/Home.jsx
+++ team-boulet/src/Components/Home/Home.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import room from '../../data';
 import CheckoutSteps from '../CheckoutSteps/CheckoutSteps';
 
 import './Home.css';
@@ -15,23 +14,21 @@ const Home = () => {
 
   return (
     <div>
-    <CheckoutSteps step1></CheckoutSteps>
-    <div className='main-container'>
- 
+      <CheckoutSteps step1></CheckoutSteps>
+      <div className='main-container'> 
 
-      <h1 className='titre-home'>What is my next DIY project?</h1>
-      <p className='txt-intro'>Get all the materials you need for your next project and tips on how to do it like a pro!</p>
-      <p className='txt-txt'>Also take advantage of our remote coaching programme so that you are never alone during your project.</p>
+        <h1 className='titre-home'>What is my next DIY project?</h1>
+        <p className='txt-intro'>Get all the materials you need for your next project and tips on how to do it like a pro!</p>
+        <p className='txt-txt'>Also take advantage of our remote coaching programme so that you are never alone during your project.</p>
 
-      <div className='rooms-container'>
-        <p className='txt-question'>Which room needs work?</p>      
-        {homerooms.map((listroom) => <button className='rooms'onClick={selectRoom} >{listroom}</button>)}
-        <Link to='/materials'><button type='submit' className='button-sub' onClick={selectLetsgo}>Let's go !</button></Link>
+        <div className='rooms-container'>
+          <p className='txt-question'>Which room needs work?</p>      
+          {homerooms.map((listroom) => <button className='rooms'onClick={selectRoom} >{listroom}</button>)}
+          <Link to='/materials'><button type='submit' className='button-sub' onClick={selectLetsgo}>Let's go !</button></Link>
+        </div>
+
       </div>
-
-    </div>
     </div>
   )};
-
 
 export default Home;
