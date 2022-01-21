74a705a43c2361c58d1306ade84c39850c29bc23
Vincent Doree <vinz77@hotmail.com>
2022-01-21 08:39:10 +0100

    patch css

--- team-boulet/src/Components/CrossSelling/CrossSelling.jsx
+++ team-boulet/src/Components/CrossSelling/CrossSelling.jsx
@@ -45,7 +45,7 @@ const CrossSelling = () => {
               <div className="option-container4">
                 <h4 className="title2">Starter Pack {el.material_name}</h4>
                 <div className="option-container3">
-                  <label for="name">I want it all</label>
+                  <label for="name1">I want it all</label>
                   <input
                     type="checkbox"
                     checked={input}
@@ -135,7 +135,7 @@ const CrossSelling = () => {
               <div className="option-container4">
                 <h4 className="title2">Starter Pack {el.material_name}</h4>
                 <div className="option-container3">
-                  <label for="name">I want it all</label>
+                  <label for="name1">I want it all</label>
                   <input
                     type="checkbox"
                     checked={input4}
@@ -226,7 +226,7 @@ const CrossSelling = () => {
               <div className="option-container4">
                 <h4 className="title2">Starter Pack {el.material_name}</h4>
                 <div className="option-container3">
-                  <label for="name">I want it all</label>
+                  <label for="name1">I want it all</label>
                   <input
                     type="checkbox"
                     checked={input14}
